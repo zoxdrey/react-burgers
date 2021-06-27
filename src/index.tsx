e21657f3517325448b572242cc4019b0cbcb10f5
zoxdrey <zoxypva@gmail.com>
2021-06-27 18:58:07 +0300

    fix import

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './services/reducers/reducers';
+import rootReducer from './services/reducers/index';
 
 const composeEnhancers =
 (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
