ac6129ab7b2dd570277db51ded692f16b9b8662a
Вячеcлав Петров <v.petrov@axelot.ru>
2021-07-16 17:51:01 +0300

    order detail template

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -1,48 +1,75 @@
 import styles from "./order-details.module.css";
 import React from "react";
-import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import {NavLink} from "react-router-dom";
+import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
 function OrderDetails() {
     return (
-        <NavLink className={`${styles["order-detail-card"]} text m-2 p-6`} to={'/2314235'}>
-            <div className={`${styles["order-detail-card__header"]} mb-6`}>
-                <div className={`${styles["order-detail-card__header-date"]}`}><p
-                    className="text text_type_main-default text_color_inactive">
-                    Сегодня, 16:20 i-GMT+3
-                </p></div>
-            </div>
-            <div className={`${styles["order-detail-card__main"]}  mb-6`}>
-                <div className={`${styles["order-detail-card__main-title"]} mb-2`}><p
-                    className="text text_type_main-medium">
-                    Death Star Starship Main бургер
-                </p></div>
-                <div className={`${styles["order-detail-card__main-state"]}`}><p
-                    className="text text_type_main-default">
-                    Создан
-                </p></div>
-                <div className={`${styles["order-detail-card-__footer-list"]} mr-6`}>
-                    <img className="img" src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/>
-                    <img className="img" src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/>
-                    <img className="img" src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/>
-                    <img className="img" src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/>
+        <NavLink className={`${styles["order-detail-card"]} `} to={'/2314235'}>
+            <div className={`${styles["order-detail-card__header"]} mb-15`}>
+                <div className={`${styles["order-detail-card__header-number"]} mb-10`}>
+                    <p className="text text_type_digits-default">#034533</p>
                 </div>
-            </div>
-            <div className={`${styles["order-detail-card__footer"]}`}>
+                <div className={`${styles["order-detail-card__header-title"]} mb-3`}>
+                    <p className="text text_type_main-medium">
+                        Black Hole Singularity острый бургер
+                    </p>
 
-                <div className={`${styles["order-detail-card__header-number"]}`}>
-                    <p
-                        className="text text_type_digits-default">
-                        #034535
+                </div>
+                <div className={`${styles["order-detail-card__header-state"]} `}>
+                    <p className="text text_type_main-default">
+                        Выполнен
+                    </p>
+
+                </div>
+            </div>
+
+            <div className={`${styles["order-detail-card__main"]} `}>
+                <div className={`${styles["order-detail-card__main-title"]} mb-6`}>
+                    <p className="text text_type_main-medium">
+                        Состав:
+                    </p>
+
+                </div>
+                <div className={`${styles["order-detail-card__main-list"]} `}>
+                    <div className={`${styles["order-detail-card__main-row"]} `}>
+                        <div className={`${styles["order-detail-card__main-row-img"]} `}>
+                            <img className="img" src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/>
+                        </div>
+                        <div className={`${styles["order-detail-card__main-row-title"]} `}>
+                            <p className="text text_type_main-default">
+                                Флюоресцентная булка R2-D3
+                            </p>
+                        </div>
+                        <div className={`${styles["order-detail-card__main-row-price"]} `}>
+                            <p className="text text_type_digits-default">1 x 510</p>
+                            <CurrencyIcon type="primary"/>
+                        </div>
+                    </div>
+                    <div className={`${styles["order-detail-card__main-row"]} `}>
+                        row
+                    </div>
+                    <div className={`${styles["order-detail-card__main-row"]} `}>
+                        row
+                    </div>
+                    <div className={`${styles["order-detail-card__main-row"]} `}>
+                        row
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${styles["order-detail-card__footer"]} `}>
+                <div className={`${styles["order-detail-card__footer-date"]} `}>
+                    <p className="text text_type_main-default text_color_inactive">
+                        Вчера, 13:50 i-GMT+3
                     </p>
                 </div>
-                <div className={`${styles["order-card__footer-price"]}`}>
-                    <p
-                        className="text text_type_digits-default">
-                        480
-                    </p>
+                <div className={`${styles["order-detail-card__footer-price"]} `}>
+                    <p className="text text_type_digits-default">510</p>
+
                     <CurrencyIcon type="primary"/>
                 </div>
+
             </div>
         </NavLink>
     );
