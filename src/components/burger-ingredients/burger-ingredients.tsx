27f6de79b1a48cc369e35bc1e67a1dbd866b2628
zoxdrey <zoxypva@gmail.com>
2021-08-07 17:50:10 +0300

    some final fixes

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,46 +1,12 @@
 import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 import {BurgerIngredientsList} from "./burger-ingredients-list/burger-ingredients-list";
 import styles from "./burger-ingredients.module.css";
-import {Modal} from "../modal/modal";
-import {IngredientDetails} from "../ingredient-details/ingredient-details";
-import React, {FC, useContext, useState} from "react";
-import {BurgersDataContext} from "../../services/burgersDataContext.js";
-import {useHistory} from "react-router-dom";
+import React, {FC, useState} from "react";
 
 export const BurgerIngredients: FC = () => {
     const [current, setCurrent] = useState("Булки");
-    let history = useHistory();
-    const [visible, setVisible] = useState(false);
-    const burgersData = useContext(BurgersDataContext);
 
 
-    let back = () => {
-        history.goBack();
-    };
-
-    const closeModal = () => {
-        setVisible(false);
-        back();
-    };
-
-    const closeByOverlayClickHandler = (e) => {
-        if (e.target.parentNode.id === "modals") {
-            back();
-            closeModal();
-
-        }
-    };
-
-    const modal = (
-        <Modal
-            title
-            burgersData={burgersData}
-            closeHandler={closeModal}
-            closeByOverlayClickHandler={closeByOverlayClickHandler}
-        >
-            <IngredientDetails/>
-        </Modal>
-    );
     return (
         <div className={`${styles["burger-ingredients"]} mt-10`}>
             <h1 className="text text_type_main-large">Соберите бургер</h1>
@@ -67,7 +33,7 @@ export const BurgerIngredients: FC = () => {
                     Начинки
                 </Tab>
             </div>
-            {visible && modal}
+
             <BurgerIngredientsList
                 setCurrentTab={setCurrent}
             />
--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -1,15 +1,18 @@
 import {getCookie} from "../../utils/cookie";
 import {
-    WS_CONNECTION_CLOSE, WS_CONNECTION_CLOSED,
+    WS_CONNECTION_CLOSE,
+    WS_CONNECTION_CLOSED,
     WS_CONNECTION_ERROR,
     WS_CONNECTION_START,
-    WS_CONNECTION_SUCCESS, WS_GET_MESSAGE, WS_SEND_MESSAGE
+    WS_CONNECTION_SUCCESS,
+    WS_GET_MESSAGE,
+    WS_SEND_MESSAGE
 } from "../constants/ingredients";
 
 
 export const socketMiddleware = (wsUrl) => {
     return store => {
-        let socket = null;
+        let socket;
 
         return next => action => {
             const {dispatch} = store;
